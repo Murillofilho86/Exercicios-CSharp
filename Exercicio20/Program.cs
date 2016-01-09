846a474929fd8cad443cbd774edb84f19952889a
Murillo Fillho <murillohard@hotmail.com>
2016-01-09 10:48:06 -0200

    Commit1.2

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Escrever um programa declarando três variáveis do tipo inteiro (a, b e c). 
+//Ler um valor maior que zero para cada variável (se o valor digitado não é válido, 
+//mostrar mensagem e ler novamente). Exibe o menor valor lido multiplicado pelo maior
+//e o maior valor dividido pelo menor.
+namespace Exercicio20
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA, valorB, valorC;
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
